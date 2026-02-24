2c74ec0e3507f69f9b146ebfee8768dd1b2dc19b
ClaraBessa <clarafernandes597@gmail.com>
2026-02-24 18:52:01 -0300

    Create anotacoes.cs

---
+++ Aula 2/anotacoes.cs
@@ -0,0 +1,40 @@
+
+/*Console.WriteLine("Bem vindo(a) ao MiniMetacritic!");
+
+
+//Estrutura em C para criar uma Struct
+struct Title
+{
+    string Name;
+
+    int UserScore;
+
+    int MetaScore;
+}
+
+//Dentro da POO, trabalhamos com classes. 
+
+-------------------------------------------------------------------------------------------------------
+
+Title novoTitulo = new() //OUUU Title novoTitulo = new title() OUUUU var novoTitulo = new Title()
+{
+    Name = "Omori",
+    ReleaseDate = new DateTime(2020, 12, 25),
+    MetaScore = 63,
+    UserScore = 63
+};
+
+//DA PRA ALTERAR O ESTADO!!! 
+novoTitulo.MetaScore = 63;
+novoTitulo.UserScore = 63;
+
+//Para deixr algo como OBRIGATÓRIO::
+
+Title novoTitulo2 = new()
+{
+    Name = "Reanimal",
+    ReleaseDate = new DateTime(2026, 02, 13),
+    MetaScore = 80,
+    UserScore = 81
+
+}; */
