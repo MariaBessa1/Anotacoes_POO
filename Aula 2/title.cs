df2bd10ae7fe7a3161dab8b2282bb790cb9e7305
ClaraBessa <clarafernandes597@gmail.com>
2026-02-24 18:52:38 -0300

    Create title.cs

---
+++ Aula 2/title.cs
@@ -0,0 +1,13 @@
+public class Title
+{
+    public string Name { get; set; } //Get e set garante que é uma **propriedade**, e não um campo.
+    //como campo:: public string Name;
+    public DateTime ReleaseData; //biblioteca para data
+    public int MetaScore;
+    public int UserScore;
+}
+
+//Classe: Representa algo do mundo real, nao necessariamente algo palpavel. Pode ser um conceito. É só a ESTRUTURA, a blueprint do que será criado 
+   //---> Publico significa que tem acesso publico no programa
+
+//struct: 
